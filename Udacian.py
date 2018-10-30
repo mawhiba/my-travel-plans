1e4a6c1b5d318c3449223c0b4b664b7207d37366
Mawhiba <m.j_2010@windowslive.com>
2018-10-30 07:15:21 +0300

    Class Udacian

---
+++ Udacian.py
@@ -0,0 +1,10 @@
+class Udacian:
+    def __init__(self,name,city,enrollment,nanodegree,status):
+        self.name = "Mawhiba"
+        self.city = "Qatif"
+        self.enrollment = ""
+        self.nanodegree = "Full Stack Web Developer"
+        self.status = ""
+
+    def print_udacian(self):
+        print(self)
