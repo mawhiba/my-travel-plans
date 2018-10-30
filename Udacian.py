01a5b0751e950f0b27d7eba680ee610d9fb4b247
Mawhiba <m.j_2010@windowslive.com>
2018-10-30 07:32:14 +0300

    Class Udacian

--- Udacian.py
+++ Udacian.py
@@ -1,10 +1,15 @@
 class Udacian:
     def __init__(self,name,city,enrollment,nanodegree,status):
-        self.name = "Mawhiba"
-        self.city = "Qatif"
-        self.enrollment = ""
-        self.nanodegree = "Full Stack Web Developer"
-        self.status = ""
+        self.name = name
+        self.city = city
+        self.enrollment = enrollment
+        self.nanodegree = nanodegree
+        self.status = status
 
     def print_udacian(self):
-        print(self)
+        print(self.name + " is enrolled in " + self.city + " studying "+
+        self.nanodegree + " with Ms. Elham," + self.enrollment +" ,she is " + self.status)
+
+udaci = Udacian("Mawhiba","Dammam","Session A.M","Full Stack Web Developer",
+"On track")
+udaci.print_udacian()
